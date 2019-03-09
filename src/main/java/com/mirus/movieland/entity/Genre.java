0a550f2b0d04f761183f177446bda8203b3a78f2
mirusnet <mirusnet@gmail.com>
2019-03-09 16:25:58 +0200

    genre funcvitonality added

---
+++ src/main/java/com/mirus/movieland/entity/Genre.java
@@ -0,0 +1,11 @@
+package com.mirus.movieland.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Genre {
+    private int id;
+    private String name;
+}
---
+++ src/main/java/com/mirus/movieland/repository/jdbc/mapper/GenreRowMapper.java
@@ -0,0 +1,17 @@
+package com.mirus.movieland.repository.jdbc.mapper;
+
+import com.mirus.movieland.entity.Genre;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GenreRowMapper implements RowMapper<Genre> {
+    @Override
+    public Genre mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Genre genre = new Genre();
+        genre.setId(resultSet.getInt("id"));
+        genre.setName(resultSet.getString("name"));
+        return genre;
+    }
+}
